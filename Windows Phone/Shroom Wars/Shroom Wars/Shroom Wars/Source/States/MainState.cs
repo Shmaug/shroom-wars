9f719c7f0ae0e42e8ced3ba8ef1313774573d473
Jay Mo <unitedjaymo@live.com>
2014-08-04 10:45:31 -0400

    Adding State Manager System

---
+++ Windows Phone/Shroom Wars/Shroom Wars/Shroom Wars/Source/States/MainState.cs
@@ -0,0 +1,39 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Audio;
+
+namespace Shroom_Wars
+{
+    public class GameState : State
+    {
+        public GameState(GraphicsDeviceManager g, ContentManager c, Viewport v) : base(g, c, v)
+        {
+        }
+
+        public override void Initialize()
+        {
+        }
+
+        public override void LoadContent()
+        {
+        }
+
+        public override State Update(GameTime gameTime)
+        {
+            return this;
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Begin();
+            spriteBatch.End();
+        }
+    }
+}
